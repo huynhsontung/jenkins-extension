1b1fbc13636c97f8780bf563dcd308e5b6f5ae7f
Tung Huynh <tung75605@gmail.com>
2024-01-08 11:08:49 -0800

    display last build

--- ui/src/job-widget.tsx
+++ ui/src/job-widget.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
-import { HealthReport, JenkinsJob } from './models/jenkins';
-import { IMAGES_URL, getProxiedRequest } from './helpers';
+import { HealthReport, JenkinsBuild, JenkinsJob } from './models/jenkins';
+import { BASE_URL, IMAGES_URL, getProxiedRequest } from './helpers';
 import { Application } from './models/models';
 import { ActionButton } from 'argo-ui/v2';
 
@@ -37,8 +37,9 @@ export interface JobWidgetProps {
 }
 
 export const JobWidget = (props: JobWidgetProps) => {
-  const [icon, setIcon] = useState(null);
-  const [healthIcons, setHealthIcons] = useState([]);
+  const [icon, setIcon] = useState<string>(null);
+  const [healthIcons, setHealthIcons] = useState<string[]>([]);
+  const [lastBuild, setLastBuild] = useState<JenkinsBuild>(null);
   const { application, job, buildAction } = props;
 
   useEffect(() => {
@@ -65,7 +66,20 @@ export const JobWidget = (props: JobWidgetProps) => {
         val.forEach(URL.revokeObjectURL);
       }
     }
-  }, [job])
+  }, [job]);
+
+  useEffect(() => {
+    // fetch last build
+    if (job.lastBuild?.url) {
+      const lastBuildUrl = new URL(job.lastBuild.url);
+      fetch(getProxiedRequest(BASE_URL + lastBuildUrl.pathname, application))
+        .then(resp => resp.ok
+          ? resp.json() as Promise<JenkinsBuild>
+          : Promise.reject(new Error(`${resp.status}: ${resp.statusText}`)))
+        .then(setLastBuild)
+        .catch(console.error);
+    }
+  }, [job.lastBuild]);
 
   return (
     <>
@@ -74,8 +88,6 @@ export const JobWidget = (props: JobWidgetProps) => {
           <div style={{ display: 'flex', alignItems: 'center' }}>
             <div style={{ marginRight: '10px' }}>
               <img src={icon} style={{ width: '32px', height: '32px' }} />
-              <br />
-              <div>proj</div>
             </div>
             <div style={{ lineHeight: '15px', display: 'flex', flexDirection: 'column' }}>
               {job.fullName.includes('/') && <span>{job.fullName.substring(0, job.fullName.lastIndexOf('/') + 1)}</span>}
@@ -94,6 +106,13 @@ export const JobWidget = (props: JobWidgetProps) => {
                 <span>{health.description}</span>
               </div>
             ))}
+            {lastBuild &&
+              <div>
+                <span>Last build: </span>
+                <a href={lastBuild.url} target='_blank' rel='noopener noreferrer'>{lastBuild.displayName}</a>
+                <span>{` (${lastBuild.result})`}</span>
+              </div>
+            }
           </div>
         </div>
         <div className='pod-view__node__container'>
